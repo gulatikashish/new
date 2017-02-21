e9672c1154297f108b9fde9275aec31b913c2fbe
gulatikashish <kgulati94@gmail.com>
2017-02-21 10:26:35 +0530

    second commmit

--- exp.js
+++ exp.js
@@ -107,6 +107,22 @@ res.send(req.body)
 
    
 })
+/////////////
+app.get('/index.htm', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.htm" );
+})
+
+app.get('/process_get', function (req, res) {
+   // Prepare output in JSON format
+   response = {
+      first_name:req.query.first_name,
+      last_name:req.query.last_name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+})
+
+
 
 
 var server = app.listen(8082, function () {
